86bcb39986277672e487550ccb7fb6dd3572cd78
Nazarii Soviak <99899134+Nazik007@users.noreply.github.com>
2022-06-22 22:08:54 +0300

    Add files via upload

---
+++ itvdn_lists_2/Zoo.java
@@ -0,0 +1,20 @@
+package itvdn_lists_2;
+
+import java.util.ArrayList;
+
+public class Zoo {
+    public static void main(String[] args) {
+        ArrayList<String> zoo = new ArrayList<>();
+        zoo.add(0,"Monkey");
+        zoo.add(1,"Dog");
+        zoo.add(2,"Cat");
+        zoo.add(3,"Mouse");
+        zoo.add(4,"Zebra");
+        zoo.add(5,"Penguin");
+        zoo.add(6,"Swallow");
+        zoo.add(7,"Pig");
+
+        System.out.println(zoo);
+
+    }
+}
